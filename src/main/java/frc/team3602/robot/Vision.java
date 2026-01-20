5d596e5630753ee4f95507409263ff9a38b392ce
aperryman99 <134285874+aperryman99@users.noreply.github.com>
2026-01-19 20:32:05 -0500

    FIxed somethigns
    didnt pull before making changes to the code good thing it was only cody adding the team36032 to the file directory that was different

--- src/main/java/frc/team3602/robot/Vision.java
+++ src/main/java/frc/team3602/robot/Vision.java
@@ -1,22 +1,22 @@
-/*
- * Copyright (C) 2026 Team 3602 All rights reserved. This work is
- * licensed under the terms of the MIT license which can be found
- * in the root directory of this project.
- */
+package frc.robot;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
+import frc.robot.LimelightHelpers.RawFiducial;
 
-package frc.team3602.robot;
-
-import frc.team3602.robot.LimelightHelpers;
-import frc.team3602.robot.LimelightHelpers.LimelightTarget_Fiducial;
-import frc.team3602.robot.LimelightHelpers.RawFiducial;
 
 public class Vision {
+    
 
     public double getDistToCamera(int id) {
-        var tags = LimelightHelpers.getRawFiducials("limelight-primary");
-        return 0;
+        var tags =  LimelightHelpers.getRawFiducials("limelight-primary");
+         return  0;
     }
 
+    public double getPose2D(int x) {
+        var x = LimelightHelpers.getPose2D("limelight-primary");
+        return x;
+    }
+    
     public double getTX() {
         return LimelightHelpers.getTX("limelight-primary");
     }
