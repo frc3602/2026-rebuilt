4dcab9867707e49190e72b858218bc13ced919a9
yooperdude <casey.anonymous@gmail.com>
2026-03-16 20:00:02 -0400

    Remove unused drivetrain controller field

--- src/main/java/frc/team3602/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/team3602/robot/subsystems/CommandSwerveDrivetrain.java
@@ -32,7 +32,6 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -40,7 +39,6 @@ import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.team3602.robot.LimelightHelpers;
 import frc.team3602.robot.Vision;
@@ -74,7 +72,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private boolean m_hasAppliedOperatorPerspective = false;
     /* Vision */
     public final Vision vision = new Vision();
-    public final CommandXboxController joystick = new CommandXboxController(0);
     // Start in normal-speed mode so the robot is driveable immediately on enable.
     public double turbo = 0.8;
     /* PID Controllers */
